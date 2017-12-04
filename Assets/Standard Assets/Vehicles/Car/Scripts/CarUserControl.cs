b30530d2e6bdd80789aa0ed8a0b45f056132aa4d
clitetailor <ducnhatphung@gmail.com>
2017-12-04 17:34:03 +0700

    inspect mesh vertices

--- Assets/Standard Assets/Vehicles/Car/Scripts/CarUserControl.cs
+++ Assets/Standard Assets/Vehicles/Car/Scripts/CarUserControl.cs
@@ -10,16 +10,17 @@ namespace UnityStandardAssets.Vehicles.Car
         private CarController m_Car; // the car controller we want to use
 
 
-        private void Awake()
-        {
-            // get the car controller
-            m_Car = GetComponent<CarController>();
-        }
+		private void Awake()
+		{
+			// get the car controller
+			m_Car = GetComponent<CarController>();
+		}
 
 
         private void FixedUpdate()
         {
             // pass the input to the car!
+
             float h = CrossPlatformInputManager.GetAxis("Horizontal");
             float v = CrossPlatformInputManager.GetAxis("Vertical");
 #if !MOBILE_INPUT
---
+++ Assets/Vertices.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vertices : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Mesh mesh = GetComponent<MeshFilter>().mesh;
+		Debug.Log(mesh.vertices.Length);
+		Vector3[] vertices = mesh.vertices;
+		int i = 0;
+		while (i < vertices.Length)
+		{
+			Debug.Log(vertices[i].x);
+			i++;
+		}
+	}
+}
