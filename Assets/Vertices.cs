7a623d05145c4474604220e971f2dfbc9e86e1e1
clitetailor <ducnhatphung@gmail.com>
2017-12-07 12:35:39 +0700

    wip: road transformation

--- Assets/Vertices.cs
+++ Assets/Vertices.cs
@@ -5,27 +5,27 @@ using UnityEngine;
 public class Vertices : MonoBehaviour
 {
 
-    public GameObject anObject;
-	public GameObject a;
-    public Collider anObjCollider;
-	public static Mesh viewedModel;
-    private Camera cam;
-    private Plane[] planes;
+    private GameObject pathPlane;
+    private Collider anObjCollider;
+	private Mesh viewedModel;
+
     // Use this for initialization
     void Start()
     {
-		anObject = GameObject.Find("path-4");
-		a = GameObject.Find("Car");
-		MeshFilter viewedModelFilter = (MeshFilter)anObject.GetComponent("MeshFilter");
-		viewedModel=viewedModelFilter.mesh;
-		Mesh mesh = GetComponent<MeshFilter>().mesh;
+		pathPlane = GameObject.Find("Plane.001");
+
+		MeshFilter viewedModelFilter = (MeshFilter) pathPlane.GetComponent("MeshFilter");
+		viewedModel = viewedModelFilter.mesh;
+		
 		Vector3[] vertices = viewedModel.vertices;
-		Debug.Log(anObject.transform.position);
+		Vector3 scale = pathPlane.transform.localScale;
+		Quaternion rotation = pathPlane.transform.localRotation;
+
 		int i = 0;
 		while (i < vertices.Length)
 		{
 			GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        	cube.transform.position = new Vector3(vertices[i].x, vertices[i].y, 0);
+        	cube.transform.position = new Vector3(vertices[i].x * scale.x + 22, 0, - vertices[i].y * scale.y);
 			i++;
 		}
 	}
