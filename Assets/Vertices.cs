bf3e17ff84af19019c132a38c9b19fe2ba95dcd0
kinknife <hieudm.coccoc>
2017-12-09 13:20:07 +0700

    find road

--- Assets/Vertices.cs
+++ Assets/Vertices.cs
@@ -16,18 +16,38 @@ public class Vertices : MonoBehaviour
 
 		MeshFilter viewedModelFilter = (MeshFilter) pathPlane.GetComponent("MeshFilter");
 		viewedModel = viewedModelFilter.mesh;
-		
 		Vector3[] vertices = viewedModel.vertices;
+        int[] triangles = viewedModel.triangles;
 		Vector3 scale = pathPlane.transform.localScale;
 		Quaternion rotation = pathPlane.transform.localRotation;
-
+        Vector3[] filteredVirtice = new Vector3[vertices.Length];
 		int i = 0;
-		while (i < vertices.Length)
-		{
-			GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        	cube.transform.position = new Vector3(vertices[i].x * scale.x + 22, 0, - vertices[i].y * scale.y);
-			i++;
+		while(i < vertices.Length) {
+			Vector3 worldPoint = transform.TransformPoint(vertices[i]);
+			Vector3 centerWorldPoint = pathPlane.transform.position;
+			float distance = Vector3.Distance(centerWorldPoint,worldPoint);
+			if(distance < 1000) {
+				if (distance > 1){
+                    filteredVirtice[i] = new Vector3(worldPoint.x, 0, worldPoint.z);
+					i++;
+				} else {
+                    i++;
+                }                    
+			} else {
+                i++;
+            }
 		}
+        for(int j = 1; j < triangles.Length; ++j) {
+            int index1 = triangles[j],
+                index2 = triangles[j-1];
+            Vector3 worldPoint = filteredVirtice[index1],
+                    worldPoint1 = filteredVirtice[index2];
+            if (worldPoint != Vector3.zero){
+                GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                cube.transform.position = new Vector3((worldPoint.x + worldPoint1.x)/2, 0, (worldPoint.z + worldPoint1.z)/2);
+			    cube.name = j.ToString();
+            }
+        }
 	}
 
 	/// <summary>
