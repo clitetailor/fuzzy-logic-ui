269bdff21103406348486ea247d957fa01d4a7f0
clitetailor <ducnhatphung@gmail.com>
2017-12-09 15:17:01 +0700

    feat: obstacle new control

--- Assets/ObstacleScript.cs
+++ Assets/ObstacleScript.cs
@@ -5,34 +5,34 @@ using UnityEngine;
 public class ObstacleScript : MonoBehaviour {
 
 	private new Camera camera;
+	private new GameObject car;
 
 	// Use this for initialization
 	void Start () {
 		camera = GetComponent<Camera>();
+		car = transform.parent.gameObject;
 	}
 
 	// Update is called once per frame
 	void Update() {
 		if (Input.GetMouseButtonDown(0)) {
-			ClickEventHandler();
+			OnMouseLeftClick();
+		}
+
+		if (Input.GetMouseButtonDown(1)) {
+			OnMouseRightClick();
 		}
 	}
 
-	void ClickEventHandler() {
+	void OnMouseLeftClick() {
 		RaycastHit hit;
 
 		Ray ray = camera.ScreenPointToRay(Input.mousePosition);
 
-		if (Physics.Raycast(ray, out hit)) {
-			if (hit.transform.name == "Plane") {
-				CreateNewObstacle(hit.point);
-			} else if (hit.transform.gameObject.tag == "Obstacle") {
-				RemoveObstacle(hit.transform.gameObject);
-			} else {
-				//Pass
-			}
-			
-		}	
+		if (Physics.Raycast(ray, out hit))
+		{
+			CreateNewObstacle(hit.point);
+		}
 	}		
 			
 	void CreateNewObstacle(Vector3 position) {
@@ -40,9 +40,27 @@ public class ObstacleScript : MonoBehaviour {
 
 		cube.transform.position = position;
 		cube.tag = "Obstacle";
+		cube.transform.rotation.SetFromToRotation(Vector3.zero, new Vector3(0, 0, camera.transform.rotation.z));
+		
+		cube.transform.rotation *= car.transform.rotation;
 
 		Renderer rend = cube.GetComponent<Renderer>();
 		rend.material.shader = Shader.Find("Standard");
+		rend.material.color = Color.blue;
+	}
+
+	void OnMouseRightClick() {
+		RaycastHit hit;
+
+		Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+		if (Physics.Raycast(ray, out hit))
+		{
+			if (hit.transform.gameObject.tag == "Obstacle")
+			{
+				RemoveObstacle(hit.transform.gameObject);
+			}
+		}
 	}
 
 	void RemoveObstacle(GameObject gameObject) {
