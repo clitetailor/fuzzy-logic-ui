f7bc5f4f7e21c994d20de1eb2eeb33ec882817c6
ThanhxD <dangxthanh96@gmail.com>
2017-12-11 05:45:35 +0700

    Update data visualize script

--- FuzzyModule/data/data-visualize.R
+++ FuzzyModule/data/data-visualize.R
@@ -1,6 +1,20 @@
+args <- commandArgs(trailingOnly=TRUE)
+if (exists(args[2]) && exists(args[3])) {
+    lon <- as.integer(args[2])
+    lat <- as.integer(args[3])
+} else {
+    lon <- 40
+    lat <- 40
+}
+
 library("plot3D")
-distance <- scan("distance-yellow.txt", what=list(data=0))$data
-angle <- scan("angle-yellow.txt", what=list(data=0))$data
-light <- scan("light-yellow.txt", what=list(data=0))$data
-speed <- scan("speed-yellow.txt", what=list(data=0))$data
-scatter3D(distance, angle, light, colvar=speed, main="Yellow light", xlab="Distance", ylab="Angle", zlab="Light time", clab="Speed")
+pdf(paste(toupper(substr(args[1], 1, 1)), tolower(substring(args[1], 2)), " plots(", lon, ", ", lat,").pdf", sep=""))
+
+distance <- scan(paste("distance-", args[1], ".txt", sep=""), what=list(data=0))$data
+angle <- scan(paste("angle-", args[1], ".txt", sep=""), what=list(data=0))$data
+light <- scan(paste("light-", args[1], ".txt", sep=""), what=list(data=0))$data
+speed <- scan(paste("speed-", args[1], ".txt", sep=""), what=list(data=0))$data
+scatter3D(distance, angle, light, colvar=speed, 
+            main=paste(toupper(substr(args[1], 1, 1)), tolower(substring(args[1], 2)), " light", sep=""), 
+            xlab="Distance", ylab="Angle", zlab="Light time", clab="Speed",
+            theta=lon, phi=lat)
