5e520ff583fcfda61c19d6328f283977d23d6ece
clitetailor <ducnhatphung@gmail.com>
2017-12-11 02:54:29 +0700

    wip: merge with detect obstacle

--- Assets/CarController.cs
+++ Assets/CarController.cs
@@ -4,225 +4,185 @@ using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 using System.Linq;
 
-[RequireComponent(typeof (UnityStandardAssets.Vehicles.Car.CarController))]
-public class CarController : MonoBehaviour {
+[RequireComponent(typeof(UnityStandardAssets.Vehicles.Car.CarController))]
+public class CarController : MonoBehaviour
+{
+    private const float mpsToMPH = 2.2369362912f;
 
-	private const float mpsToMPH = 2.2369362912f;
-
-	UnityStandardAssets.Vehicles.Car.CarController m_Car;
-	Vertices road;
+    UnityStandardAssets.Vehicles.Car.CarController m_Car;
+    Vertices road;
     List<Vector3> trafficLightsFound = new List<Vector3>();
-    List<Vector3> obstaclesFound = new List<Vector3>();
+    List<Vector3> obstaclesOnScreen = new List<Vector3>();
 
-	Transform target;
-	Vector3 offsetTargetPos;
-	new Rigidbody rigidbody;
+    Transform target;
+    Vector3 offsetTargetPos;
+    new Rigidbody rigidbody;
 
-	void Start()
-	{
-		GameObject path = GameObject.Find("Path").gameObject;
-		road = path.GetComponent<Vertices>();
+    void Start()
+    {
+        GameObject path = GameObject.Find("Path").gameObject;
+        road = path.GetComponent<Vertices>();
 
-		transform.position = road.trackedPoints[0];
-		Vector3 direction = road.trackedPoints[1] - road.trackedPoints[0];
-		transform.rotation = Quaternion.LookRotation(direction);
+        transform.position = road.trackedPoints[0];
+        Vector3 direction = road.trackedPoints[1] - road.trackedPoints[0];
+        transform.rotation = Quaternion.LookRotation(direction);
 
-		rigidbody = GetComponent<Rigidbody>();
-        detectLight();
-	}
-
-	/// <summary>
-	/// Update is called every frame, if the MonoBehaviour is enabled.
-	/// </summary>
-	void getObjectInView()
-	{
-        obstaclesFound = new List<Vector3>();
-		GameObject[] obstacles = GameObject.FindGameObjectsWithTag("Obstacle");
-		RaycastHit hit;
-		for(var i = 0; i < obstacles.Length; i++)
-		{
-            Vector3 screenPoint = Camera.main.WorldToScreenPoint(obstacles[i].transform.position);
-            Ray ray = Camera.main.ScreenPointToRay(screenPoint);
-			if(Physics.Raycast(ray, out hit))
-			{
-				if(isObstacleInRoad(obstacles[i].transform.position)){
-                    obstaclesFound.Add(obstacles[i].transform.position);
-                }
-			}
-		}
-	}
-
-	bool isObstacleInRoad(Vector3 obstaclePoint) {
-        float minDistance = Vector3.Distance(obstaclePoint,road.trackedPoints[0]);
-        for(int i = 1; i < road.trackedPoints.Count; ++i) {
-            float curr = Vector3.Distance(obstaclePoint,road.trackedPoints[i]);
-            if(curr < minDistance) {
-                minDistance = curr;
-            }
-        }
-        if(minDistance > (road.roadWidth/2)) {
-            return false;
-        }
-        return true;
+        rigidbody = GetComponent<Rigidbody>();
+        DetectLight();
     }
 
-	void detectLight() {
-		GameObject[] traficLights =  GameObject.FindGameObjectsWithTag("TrafficLight");
+    private GameObject NearestObstacle()
+    {
+        obstaclesOnScreen = new List<Vector3>();
+
+        List<GameObject> obstacles = new List<GameObject>(GameObject.FindGameObjectsWithTag("Obstacle"));
         RaycastHit hit;
 
-        foreach (GameObject trafficLight in traficLights) {
-            Vector3 screenPoint = Camera.main.WorldToScreenPoint(trafficLight.transform.position);
+        float minDist = Mathf.Infinity;
+        GameObject nearestObstacle = null;
+        foreach (GameObject obstacle in obstacles)
+        {
+            Vector3 screenPoint = Camera.main.WorldToScreenPoint(obstacle.transform.position);
             Ray ray = Camera.main.ScreenPointToRay(screenPoint);
 
-			if (Physics.Raycast(ray, out hit))
-			{
-                if (trafficLight.GetComponent<TrafficLightBehavior>().lightColor != 0){
-                    trafficLightsFound.Add(getLightTrack(trafficLight.transform.position, trafficLight.transform.forward));
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (IsInTheRoad(obstacle.transform.position))
+                {
+                    float dist = Vector3.Distance(obstacle.transform.position, transform.position);
+                    if (dist < minDist)
+                    {
+                        minDist = dist;
+                        nearestObstacle = obstacle;
+                    }
                 }
-			}
-
-            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-		    cube.transform.position = getLightTrack(trafficLight.transform.position, trafficLight.transform.forward);
+            }
         }
-	}
 
-    Vector3 getLightTrack(Vector3 lightPosition,Vector3 rotation) {
+        return nearestObstacle;
+    }
+
+    private bool IsInTheRoad(Vector3 pos)
+    {
+        List<Collider> colliders = new List<Collider>(Physics.OverlapSphere(pos, road.roadWidth / 2));
+
+        foreach (Collider collider in colliders)
+        {
+            if (collider.name == "TrackedPoint")
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void DetectLight()
+    {
+        GameObject[] traficLights = GameObject.FindGameObjectsWithTag("TrafficLight");
+        RaycastHit hit;
+
+        foreach (GameObject trafficLight in traficLights)
+        {
+        Vector3 screenPoint = Camera.main.WorldToScreenPoint(trafficLight.transform.position);
+        Ray ray = Camera.main.ScreenPointToRay(screenPoint);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (trafficLight.GetComponent<TrafficLightBehavior>().lightColor != 0)
+            {
+                trafficLightsFound.Add(TrafficLightTrackPoint(trafficLight.transform.position, trafficLight.transform.forward));
+            }
+        }
+
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube.transform.position = TrafficLightTrackPoint(trafficLight.transform.position, trafficLight.transform.forward);
+        }
+    }
+
+    Vector3 TrafficLightTrackPoint(Vector3 lightPosition, Vector3 rotation)
+    {
         Vector3 lightPoint = road.trackedPoints[0];
-        float minDist = Vector3.Distance(lightPosition,road.trackedPoints[0]);
+        float minDist = Vector3.Distance(lightPosition, road.trackedPoints[0]);
 
-        for (int i = 1; i < road.trackedPoints.Count; ++i) {
+        for (int i = 1; i < road.trackedPoints.Count; ++i)
+        {
             Vector3 currPoint = road.trackedPoints[i];
             float curr = -rotation.z * (currPoint.x - lightPosition.x) + rotation.x * (currPoint.z - lightPosition.z);
 
-            if (Mathf.Abs(curr) < 0.5) {
-                float currDist = Vector3.Distance(lightPosition,currPoint);
-                if(currDist < minDist) {
-                    lightPoint = currPoint;
-                    minDist = currDist;
+            if (Mathf.Abs(curr) < 0.5)
+            {
+                float currDist = Vector3.Distance(lightPosition, currPoint);
+                if (currDist < minDist)
+                {
+                lightPoint = currPoint;
+                minDist = currDist;
                 }
             }
         }
         return lightPoint;
     }
 
-	void Awake() {
-		m_Car = GetComponent<UnityStandardAssets.Vehicles.Car.CarController>();
-	}
+    void Awake()
+    {
+        m_Car = GetComponent<UnityStandardAssets.Vehicles.Car.CarController>();
+    }
 
-	/*
-	void FixedUpdate()
-	{
-		Vector3 fwd = transform.forward;
-		if (rigidbody.velocity.magnitude > m_Car.MaxSpeed * 0.1f)
-		{
-			fwd = rigidbody.velocity;
-		}
 
-		float desiredSpeed = m_Car.MaxSpeed;
+    private void FixedUpdate()
+    {
+        // pass the input to the car!
+        List<GameObject> trackPoints;
+        trackPoints = FrontTrackPoints(10);
+        float desiredAngle = RotateAngle(trackPoints);
 
-		// now it's time to decide if we should be slowing down...
-		// the car will brake according to the upcoming change in direction of the target. Useful for route-based AI, slowing for corners.
+        float h = desiredAngle / 30;
+        //float h = CrossPlatformInputManager.GetAxis("Horizontal");
+        float v = CrossPlatformInputManager.GetAxis("Vertical");
 
-		// check out the angle of our target compared to the current direction of the car
-		float approachingCornerAngle = Vector3.Angle(target.forward, fwd);
 
-		// also consider the current amount we're turning, multiplied up and then compared in the same way as an upcoming corner angle
-		float spinningAngle = rigidbody.angularVelocity.magnitude * cautiousAngularVelocityFactor;
+    #if !MOBILE_INPUT
+        float handbrake = CrossPlatformInputManager.GetAxis("Jump");
+        m_Car.Move(h, v, v, handbrake);
+    #else
+            m_Car.Move(h, v, v, 0f);
+    #endif
+    }
 
-		// if it's different to our current angle, we need to be cautious (i.e. slow down) a certain amount
-		float cautiousnessRequired = Mathf.InverseLerp(0, cautiousMaxAngle,
-														Mathf.Max(spinningAngle,
-																	approachingCornerAngle));
+    private List<GameObject> FrontTrackPoints(int radius)
+    {
+        List<Collider> colliders = new List<Collider>(Physics.OverlapSphere(transform.position, radius));
 
-		desiredSpeed = Mathf.Lerp(m_Car.MaxSpeed, m_Car.MaxSpeed * m_CautiousSpeedFactor,
-									cautiousnessRequired);
+        return colliders
+        .Where(collider =>
+        {
+            Vector3 colliderOffset = collider.transform.position - transform.position;
+            Quaternion lookRotation = Quaternion.LookRotation(colliderOffset);
 
-		// use different sensitivity depending on whether accelerating or braking:
-		float accelBrakeSensitivity = (desiredSpeed < m_Car.CurrentSpeed)
-											? m_BrakeSensitivity
-											: m_AccelSensitivity;
-
-		// decide the actual amount of accel/brake input to achieve desired speed.
-		float accel = Mathf.Clamp((desiredSpeed - m_Car.CurrentSpeed) * accelBrakeSensitivity, -1, 1);
-
-		// calculate the local-relative position of the target, to steer towards
-		Vector3 localTarget = transform.InverseTransformPoint(offsetTargetPos);
-
-		// work out the local angle towards the target
-		float targetAngle = Mathf.Atan2(localTarget.x, localTarget.z) * Mathf.Rad2Deg;
-
-		// get the amount of steering needed to aim the car towards the target
-		float steer = Mathf.Clamp(targetAngle * m_SteerSensitivity, -1, 1) * Mathf.Sign(m_Car.CurrentSpeed);
-
-		// feed input to the car controller.
-		m_Car.Move(steer, accel, accel, 0f);
-	}
-	*/
-
-	
-	private void FixedUpdate()
-	{
-		// pass the input to the car!
-		List<GameObject> trackPoints;
-		trackPoints = FrontTrackPoints(10);
-		float desiredAngle = RotateAngle(trackPoints);
-		//float deltaAngle = desiredAngle - m_Car.CurrentSteerAngle;
-
-		//Debug.Log("Current Angle: " + m_Car.CurrentSteerAngle);
-		//Debug.Log("Desired Angle: " + desiredAngle);
-		//Debug.Log("Delta Angle:   " + deltaAngle);
-
-		//float h1 = deltaAngle / 90;
-		float h = desiredAngle / 30;
-		//float h = CrossPlatformInputManager.GetAxis("Horizontal");
-		//float h = - desiredAngle / 90;
-		float v = CrossPlatformInputManager.GetAxis("Vertical");
-
-		//Debug.Log("h:             " + h);
-		//Debug.Log("h1:            " + h1);
-		//Debug.Log("h2:            " + h2);
-
-		//Debug.Log(h);
-
-#if !MOBILE_INPUT
-		float handbrake = CrossPlatformInputManager.GetAxis("Jump");
-		m_Car.Move(h, v, v, handbrake);
-#else
-        m_Car.Move(h, v, v, 0f);
-#endif
-	}
-
-	private List<GameObject> FrontTrackPoints(int radius) {
-		List<Collider> colliders = new List<Collider>(Physics.OverlapSphere(transform.position, radius));
-
-		return colliders
-			.Where(collider =>
-			{
-				Vector3 colliderOffset = collider.transform.position - transform.position;
-				Quaternion lookRotation = Quaternion.LookRotation(colliderOffset);
-
-				return collider.gameObject.name == "TrackedPoint"
+            return collider.gameObject.name == "TrackedPoint"
 				&& Quaternion.Angle(lookRotation, transform.rotation) < 90
 				&& Vector3.Distance(transform.position, collider.gameObject.transform.position) > 0f;
-			})
-			.Select(collider => collider.gameObject)
-			.ToList();
-	}
+        })
+        .Select(collider => collider.gameObject)
+        .ToList();
+    }
 
-	private float RotateAngle(List<GameObject> trackPoints) {
-		List<float> amounts = trackPoints
+    private float RotateAngle(List<GameObject> trackPoints)
+    {
+        List<float> amounts = trackPoints
 			.Select(point =>
 				Vector3.Distance(point.transform.position, transform.position))
-			.Select(length => 1f / ( Mathf.Abs(length - 5) + 1 ))
+			.Select(length => 1f / (Mathf.Abs(length - 5) + 1))
 			.ToList();
 
-		float sum = 0;
-		for (int i = 0; i < amounts.Count; ++i) {
+        float sum = 0;
+        for (int i = 0; i < amounts.Count; ++i)
+        {
 			sum += amounts[i];
-		}
+        }
 
-		float accum = 0;
-		for (int i = 0; i < trackPoints.Count; ++i) {
+        float accum = 0;
+        for (int i = 0; i < trackPoints.Count; ++i)
+        {
 			Quaternion rotation = Quaternion.LookRotation(trackPoints[i].transform.position - transform.position);
 			float carY = transform.rotation.eulerAngles.y;
 			float roadY = rotation.eulerAngles.y;
@@ -233,20 +193,23 @@ public class CarController : MonoBehaviour {
 			float deltaY = roadY - carY;
 			Modular(ref deltaY);
 			accum += deltaY * amounts[i] / sum;
-		}
+        }
 
-		float angle = accum;
-		
-		return angle;
-	}
+        float angle = accum;
 
-	private void Modular(ref float angle) {
-		while (angle < -180f) {
-			angle += 360f;
-		}
+        return angle;
+    }
 
-		while (angle > 180f) {
-			angle -= 360f;
-		}
-	}
+    private void Modular(ref float angle)
+    {
+        while (angle < -180f)
+        {
+        angle += 360f;
+        }
+
+        while (angle > 180f)
+        {
+        angle -= 360f;
+        }
+    }
 }
--- Assets/Vertices.cs
+++ Assets/Vertices.cs
@@ -4,137 +4,162 @@ using UnityEngine;
 using System.Linq;
 using System;
 
-public class Vertices : MonoBehaviour {
+public class Vertices : MonoBehaviour
+{
     private GameObject pathPlane;
-	private Mesh roadMesh;
+    private Mesh roadMesh;
     private int MAX_TRAFFIC_LIGHT = 4;
 
-	[HideInInspector] public List<Vector3> trackedPoints = new List<Vector3>();
-	[HideInInspector] public List<GameObject> trackedNavigators = new List<GameObject>();
+    [HideInInspector] public List<Vector3> trackedPoints = new List<Vector3>();
+    [HideInInspector] public List<GameObject> trackedNavigators = new List<GameObject>();
 
-	[HideInInspector]
-	public List<List<Vector3>> vertexTriples = new List<List<Vector3>>();
+    [HideInInspector]
+    public List<List<Vector3>> vertexTriples = new List<List<Vector3>>();
     public float roadWidth;
 
-	// Use this for initialization
-	void Start() {
-		pathPlane = GameObject.Find("Plane.001");
+    // Use this for initialization
+    void Start()
+    {
+    pathPlane = GameObject.Find("Plane.001");
 
-		roadMesh = pathPlane.GetComponent<MeshFilter>().mesh;
-		List<Vector3> vertices = new List<Vector3>(roadMesh.vertices);
-        List<int> triangles = new List<int>(roadMesh.triangles);
-		
-		// To absolute positions
-		vertices = vertices
-			.Select(vertex => pathPlane.transform.TransformPoint(vertex))
-			.ToList();
+    roadMesh = pathPlane.GetComponent<MeshFilter>().mesh;
+    List<Vector3> vertices = new List<Vector3>(roadMesh.vertices);
+    List<int> triangles = new List<int>(roadMesh.triangles);
 
-        List<Vector3> trafficLights = getExistedTrafficLight(vertices);
-        randomTrafficLight(trafficLights, vertices);
+    // To absolute positions
+    vertices = vertices
+        .Select(vertex => pathPlane.transform.TransformPoint(vertex))
+        .ToList();
 
-		int numberOfTriangles = triangles.Count / 3;
+    List<Vector3> trafficLights = GetExistingTrafficLights(vertices);
+    CreateRandomTrafficLights(ref trafficLights, vertices);
 
-		for (int i = 0; i < numberOfTriangles; ++i) {
-			int startIndex = i * 3;
+    int numberOfTriangles = triangles.Count / 3;
 
-			List<Vector3> triple = new List<Vector3>(new Vector3[] {
-				vertices[ triangles[startIndex] ],
-				vertices[ triangles[startIndex + 1] ],
-				vertices[ triangles[startIndex + 2] ]
-			});
+    for (int i = 0; i < numberOfTriangles; ++i)
+    {
+        int startIndex = i * 3;
 
-			vertexTriples.Add(triple);
-		}
+        List<Vector3> triple = new List<Vector3>(new Vector3[] {
+        vertices[ triangles[startIndex] ],
+        vertices[ triangles[startIndex + 1] ],
+        vertices[ triangles[startIndex + 2] ]
+        });
 
-		for (int i = 0; i < numberOfTriangles; ++i)
-		{
-			List<Vector3> curr = vertexTriples[i];
-			List<Vector3> next = vertexTriples[(i + 1) % numberOfTriangles];
-
-			// Find the shared edge of two consecutive triangles
-			List<Vector3> edgePoints = new List<Vector3>();
-			foreach (Vector3 vertex in next) {
-				if (curr.IndexOf(vertex) != -1) {
-					edgePoints.Add(vertex);
-				}
-			}
-
-			// Find the edge midpoint
-			if (edgePoints.Count == 2) {
-                for(int j = 1; j < trafficLights.Count; ++j) {
-                    if(edgePoints[0] == trafficLights[j]) {
-                        Vector3 position = edgePoints[0];
-                        Vector3 rotation;
-                        rotation = edgePoints[1] - edgePoints[0];
-                        Instantiate(GameObject.FindGameObjectsWithTag("TrafficLight")[0], position, Quaternion.LookRotation(rotation));
-                        trafficLights[j] = Vector3.zero;
-                    } 
-                    if(edgePoints[1] == trafficLights[j]) {
-                        Vector3 position = edgePoints[1];
-                        Vector3 rotation;
-                        rotation = edgePoints[0] - edgePoints[1];
-                        Instantiate(GameObject.FindGameObjectsWithTag("TrafficLight")[0], position, Quaternion.LookRotation(rotation));
-                        trafficLights[j] = Vector3.zero;
-                    }
-                }
-				trackedPoints.Add((edgePoints[0] + edgePoints[1]) / 2);
-                float currWidth = Vector3.Distance(edgePoints[0],edgePoints[1]);
-                if(roadWidth < currWidth) {
-                    roadWidth = currWidth;
-                }
-			}
-		}
-
-		for (int i = 0; i < trackedPoints.Count; ++i) {
-			Vector3 currPoint = trackedPoints[i];
-			Vector3 nextPoint = trackedPoints[(i + 1) % trackedPoints.Count];
-
-			GameObject navigator = new GameObject("TrackedPoint");
-			navigator.transform.position = currPoint;
-			navigator.transform.rotation = Quaternion.LookRotation(nextPoint - currPoint);
-			navigator.AddComponent<BoxCollider>();
-			Collider collider = navigator.GetComponent<BoxCollider>();
-			collider.isTrigger = true;
-
-			trackedNavigators.Add(navigator);
-		}
-	}
-
-    List<Vector3> getExistedTrafficLight(List<Vector3> vertices) {
-        GameObject[] trafficLights = GameObject.FindGameObjectsWithTag("TrafficLight");
-        List<Vector3> trafficPosition = new List<Vector3>();
-        for(int i = 0; i < trafficLights.Length;++i) {
-            trafficPosition.Add(findTrafficLight(trafficLights[i].transform.position,vertices));
-        }
-        return trafficPosition;
+        vertexTriples.Add(triple);
     }
 
-    Vector3 findTrafficLight(Vector3 lightPosition,List<Vector3> vertices) {
-        Vector3 position = Vector3.zero;
-        float minDist = Mathf.Infinity;
-        for(int i = 0; i < vertices.Count; ++i) {
-            float currDist = Vector3.Distance(lightPosition,vertices[i]);
-            if(currDist < minDist) {
-                minDist = currDist;
-                position = vertices[i];
+    for (int i = 0; i < numberOfTriangles; ++i)
+    {
+        List<Vector3> curr = vertexTriples[i];
+        List<Vector3> next = vertexTriples[(i + 1) % numberOfTriangles];
+
+        // Find the shared edge of two consecutive triangles
+        List<Vector3> edgePoints = new List<Vector3>();
+        foreach (Vector3 vertex in next)
+        {
+        if (curr.IndexOf(vertex) != -1)
+        {
+            edgePoints.Add(vertex);
+        }
+        }
+
+        // Find the edge midpoint
+        if (edgePoints.Count == 2)
+        {
+        for (int j = 1; j < trafficLights.Count; ++j)
+        {
+            if (edgePoints[0] == trafficLights[j])
+            {
+            Vector3 position = edgePoints[0];
+            Vector3 rotation;
+            rotation = edgePoints[1] - edgePoints[0];
+            Instantiate(GameObject.FindGameObjectsWithTag("TrafficLight")[0], position, Quaternion.LookRotation(rotation));
+            trafficLights[j] = Vector3.zero;
+            }
+            if (edgePoints[1] == trafficLights[j])
+            {
+            Vector3 position = edgePoints[1];
+            Vector3 rotation;
+            rotation = edgePoints[0] - edgePoints[1];
+            Instantiate(GameObject.FindGameObjectsWithTag("TrafficLight")[0], position, Quaternion.LookRotation(rotation));
+            trafficLights[j] = Vector3.zero;
             }
         }
-        return position;
+        trackedPoints.Add((edgePoints[0] + edgePoints[1]) / 2);
+
+        float currWidth = Vector3.Distance(edgePoints[0], edgePoints[1]);
+        if (roadWidth < currWidth)
+        {
+            roadWidth = currWidth;
+        }
+        }
     }
 
-    void randomTrafficLight(List<Vector3> trafficLights,List<Vector3> vertices) {
-        while(trafficLights.Count < MAX_TRAFFIC_LIGHT) {
-            bool add = true;
-            int random = UnityEngine.Random.Range(0,vertices.Count);
-            for(int i = 0; i < trafficLights.Count;++i) {
-                float dist = Vector3.Distance(trafficLights[i],vertices[random]);
-                if(dist < 50) {
-                    add = false;
-                }
-            }
-            if(add){
-                trafficLights.Add(vertices[random]);
-            }
+    for (int i = 0; i < trackedPoints.Count; ++i)
+    {
+        Vector3 currPoint = trackedPoints[i];
+        Vector3 nextPoint = trackedPoints[(i + 1) % trackedPoints.Count];
+
+        GameObject navigator = new GameObject("TrackedPoint");
+        navigator.transform.position = currPoint;
+        navigator.transform.rotation = Quaternion.LookRotation(nextPoint - currPoint);
+        navigator.AddComponent<BoxCollider>();
+        Collider collider = navigator.GetComponent<BoxCollider>();
+        collider.isTrigger = true;
+
+        trackedNavigators.Add(navigator);
+    }
+    }
+
+    List<Vector3> GetExistingTrafficLights(List<Vector3> vertices)
+    {
+    List<GameObject> trafficLights = new List<GameObject>(GameObject.FindGameObjectsWithTag("TrafficLight"));
+    List<Vector3> trafficPositions = new List<Vector3>();
+
+    foreach (GameObject trafficLight in trafficLights)
+    {
+        trafficPositions.Add(FindTrafficLightPos(trafficLight.transform.position, vertices));
+    }
+    return trafficPositions;
+    }
+
+    Vector3 FindTrafficLightPos(Vector3 lightPosition, List<Vector3> vertices)
+    {
+    Vector3 position = Vector3.zero;
+    float minDist = Mathf.Infinity;
+    
+    foreach (Vector3 vertex in vertices)
+    {
+        float currDist = Vector3.Distance(lightPosition, vertex);
+        if (currDist < minDist)
+        {
+        minDist = currDist;
+        position = vertex;
         }
+    }
+    return position;
+    }
+
+    void CreateRandomTrafficLights(ref List<Vector3> trafficLights, List<Vector3> vertices)
+    {
+    while (trafficLights.Count < MAX_TRAFFIC_LIGHT)
+    {
+        bool add = true;
+        int random = UnityEngine.Random.Range(0, vertices.Count);
+        for (int i = 0; i < trafficLights.Count; ++i)
+        {
+        float dist = Vector3.Distance(trafficLights[i], vertices[random]);
+        if (dist < 50)
+        {
+            add = false;
+        }
+        }
+
+        if (add)
+        {
+        trafficLights.Add(vertices[random]);
+        }
+    }
     }
 }
